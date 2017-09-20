4ef50ad3a95f7dd8f41f7a08d3dd73c114ceb09d
Nameless-sama <anamelessdeath@gmail.com>
2017-09-20 15:18:09 -0500

    Se realizó la versión preliminar de la base de datos de la aplicación de seguimiento de obra

---
+++ shimazu/documentation/SQL/[VIEW]user_groups.sql
@@ -0,0 +1,17 @@
+USE `cdmx_obras`;
+CREATE  OR REPLACE VIEW `user_groups` AS
+(SELECT 
+    `knights`.`user_name`, 
+    `usuarios`.`nombre`, 
+    `knights_groups`.`group_name` 
+FROM 
+    `usuarios`,
+    `knights`,
+    `knights_groups`,
+    `knights_ranking` 
+WHERE 
+    `usuarios`.`knight_id` = `knights`.`knight_id` 
+    AND 
+    `knights_groups`.`group_id` = `knights_ranking`.`group_id` 
+    AND 
+    `knights`.`knight_id`=`knights_ranking`.`knight_id`)
