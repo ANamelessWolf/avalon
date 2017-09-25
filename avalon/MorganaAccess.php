80d83f1571d925ff3a48fb6edd212cf04e168ed1
Nameless-sama <anamelessdeath@gmail.com>
2017-09-25 14:12:54 -0500

    Correción de la validación de grupos

--- avalon/MorganaAccess.php
+++ avalon/MorganaAccess.php
@@ -80,7 +80,7 @@ class MorganaAccess
         //First check if is about a level access
         else if (array_key_exists($group_name, $this->groups) && isset($_SESSION[USR_ACCESS_SESSION])) {
             $grps = $_SESSION[USR_ACCESS_SESSION];
-            return in_array($this->groups, $group_name);
+            return in_array($group_name, $this->groups);
         }
         //Now check group access
         else if (isset($_SESSION[GRP_SESSION])) {
